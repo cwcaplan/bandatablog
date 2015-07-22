483d637b8170df8c44bf9c406c54a556984a3800
seth127 <seth127@gmail.com>
2015-07-22 07:47:44 -0400

    P and J thru 2007

--- Pazz and Jop/p and j digester.R
+++ Pazz and Jop/p and j digester.R
@@ -1,15 +1,14 @@
 setwd("C:/Users/Seth/Documents/bandatablog/Pazz and Jop")
 
 #year <- as.character(as.numeric(year)+1)
-year <- "1994"
+#year <- "1994"
 #load data
-pj <- read.delim(paste("pazz and jop ", year, ".txt", sep=""), 
-                       header=F, stringsAsFactors=F)
+#pj <- read.delim(paste("pazz and jop ", year, ".txt", sep=""), 
+#                       header=F, stringsAsFactors=F)
 
 pj <- list()
-pow <- data.frame()
 pjM <- data.frame()
-year <- "1974"
+year <- "1973"
 for (i in 1:34) {
     #set year
     year <- as.character(as.numeric(year)+1)
@@ -18,29 +17,54 @@ for (i in 1:34) {
                            header=F, stringsAsFactors=F)
     #deal with asterisks
     pj[[i]] <- pj[[i]][,1:4]
+    #add year
     pj[[i]]$year <- year
-    pjM <- rbind(pjM, PJ[[i]])
+    pjM <- rbind(pjM, pj[[i]])
 }
 #write.csv(wnoM, "weekly number ones.csv", row.names=F)
 
+#for (i in 1:length(pj)) {
+#    pjM <- rbind(pjM, pj[[i]])
+#}
 
-### FAILED ATTEMPT TO DEBUG
-pj <- list()
-pow <- data.frame()
-pjM <- data.frame()
-year <- "1974"
-for (i in 1:34) {
-    #set year
-    year <- as.character(as.numeric(year)+1)
-    #load data
-    pj[[i]] <- read.delim(paste("pazz and jop ", year, ".txt", sep=""), 
-                          header=F, stringsAsFactors=F)
-    #deal with asterisks
-    #doesn't work #pj[[i]] <- pj[[i]][,1:4]
-    YEAR <- rep(year, nrow(pj[[i]]))
-    pow <- cbind(YEAR, pj[[i]])
-    pow <- pow[,1:5]
-    #pj[[i]]$year <- year
-    pjM <- rbind(pjM, pow)
+#get rid of rogue asterisks
+pjM <- pjM[!pjM$V1=="*", ]
+pjM <- pjM[!pjM$V1=="**", ]
+
+#fix rows that we screwed up by asterisks
+for (i in 1:nrow(pjM)) {
+    if(grepl("^[^0-9+\\.]", pjM[i,1])) {
+        pjM[i,4] <- pjM[i,3]
+        pjM[i,3] <- pjM[i,2]
+        pjM[i,2] <- pjM[i,1]
+        pjM[i,1] <- as.character(as.numeric(pjM[i-1,1])+1)
+    }
 }
-#write.csv(wnoM, "weekly number ones.csv", row.names=F)
+
+#split artist and album columns
+albums <- character()
+for (i in 1:nrow(pjM)) {
+    if(grepl(":", pjM[i,2])) {
+        info <- unlist(strsplit(pjM[i,2], ": "))
+        pjM[i,2] <- info[1]
+        albums[i] <- info[2]
+    } else {
+        albums[i] <- pjM[i,2]
+    } 
+}
+
+pjM <- data.frame(rank=pjM$V1, 
+                  artist=pjM$V2, 
+                  album=albums,
+                  points=pjM$V3,
+                  mentions=pjM$V4,
+                  year=pjM$year,
+                  stringsAsFactors=F)
+
+#make rank numeric
+pjM$rank <- as.numeric(pjM$rank)
+
+#makes the year Date format, but just gives it whatever day it is currently
+#pjM$year <- as.Date(pjM$year, "%Y")
+
+#write.csv(pjM, "pazz and jop 1974-2007.csv", row.names=F)
